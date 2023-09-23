17bba24f58afe40e1b1e3f93e5dc72a9ea9714c2
Yasaswini <yasaswini.k@kmit.edu.in>
2023-09-23 12:47:36 +0530

    Maven pipeline

---
+++ src/main/java/TeamSE/sample/App.java
@@ -0,0 +1,15 @@
+package TeamSE.sample;
+
+/**
+ * Addition of two numbers
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=5, b=6;
+       int c=a+b;
+    	System.out.println( c );
+    }
+}
